369ff4c7bdc8ca6ed8342cb03b73203b5f29dd1e
handsomeboyHT <ht2631503150@gmail.com>
2019-12-03 20:08:26 +0800

    init project

---
+++ dist/precache-manifest.6b267f7694aa02bd6fc4e21201adc99c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f74d203cadfe02fab3",
+    "url": "/css/app.9427d9b4.css"
+  },
+  {
+    "revision": "3fc0457912f7e505ec07f766f6073c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4f74d203cadfe02fab3",
+    "url": "/js/app.c32e0ec8.js"
+  },
+  {
+    "revision": "f90f8636e5488bc47a87",
+    "url": "/js/chunk-vendors.85e588f9.js"
+  },
+  {
+    "revision": "635cb329039a25b52b920813654e8d6b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
